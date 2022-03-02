87d0ba3cac01f0c8ed0c1b48456de34e3d638fd8
Vincent Cramer <vincent@vdl.local>
2022-03-02 08:43:43 +0100

    Klanten CRUD

--- v1/src/api/optie/routes/optie.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * optie router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::optie.optie');
--- v1/src/api/optie/services/optie.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * optie service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::optie.optie');
